7a5c79efc0a57b9d542cd6da49f37887d563682b
uchenna-dev <meacessories.stores@gmail.com>
2022-07-03 09:39:06 +0100

    remaining tasks

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pthon3[D[D[D[D[Dy[C[C[C[D[D[Dt[C[C[C[D[D[Dhon3
+# 102-magic_calculation.py
+#Yovo Koffi Vianney
+
+
+def magic_calculation(a, b):
+"""Match bytecode provided by Holberton School, """[D[D.[[C[C[C[C
+from magic_calculation_102 import add, sub
+
+if a < b:
+c = add(a, b)
+for i in range(4, 6):
+c = add (c, i)
+return (c)
+
+else:
+return(sub(a, b))
---
+++ 0x02-python-import_modules/calculator_1.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+def add(a, b):
+"""My addition function
+Args:
+a: first integer
+b: second integer
+
+Returns:
+The return value. a + b
+"""
+return (a + b)
+
+def sub(a, b):
+"""My subtraction function
+Args:
+a: first integer
+b: second integer
+
+Returns:
+The return value. a - b
+"""
+return (a - b)
+
+def mul(a, b):
+""" My[D[[C[CMy multiplication function
+Args:
+a: first integer
+b: second integer
+
+Return:
+The return value. a * b
+"""
+return (a * b)
+
+def div(a, b):
+"""My division function
+Args:
+a: first integer
+b: second integer
+
+Returns:
+The return value. a / b
+"""
+return (a / b)[D[D[D[D[D[D[Dint[C[D(a/b)[D[D[D / b)
